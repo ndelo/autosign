8753db0e2e0cac08e913647e09d3a0c4f1432d44
ndelo <ndelo@princeton.edu>
2017-07-18 15:17:33 -0400

    Support added for AWS
    Suppert added for AWS. Also some minor changes, and changes to the
    config.yml file

--- autosign.py
+++ autosign.py
@@ -1,23 +1,21 @@
 import sys
-import boto
+import ssl
+import atexit
+import boto3.ec2
 from pyVim.connect import SmartConnect, Disconnect
 from pyVmomi import vim
 import OpenSSL.crypto
 from OpenSSL.crypto import load_certificate_request, FILETYPE_PEM
 import yaml
-import ssl 
-import atexit
 
 def main():
 
 	puppet_node = sys.argv[1].lower()[:sys.argv[1].lower().index('.')]
-	
-	if puppet_node.endswith('princeton.edu'):
+	domain = "princeton.edu"
+
+	# we want just the host name, not the fqdn
+	if puppet_node.endswith(domain):
 		puppet_node = puppet_node[:puppet_node.index('.')]
-	
-	# disable SSL certificate verification for self-signed certificates on VSphere server
-	context = ssl.SSLContext(ssl.PROTOCOL_TLSv1) 
-	context.verify_mode = ssl.CERT_NONE 
 
 	# load CSR from stdin
 	csr_from_stdin = sys.stdin.read()
@@ -26,23 +24,26 @@ def main():
 	# NOTE: Ideally we could filter by OID number, but OpenSSL.crypto.X509Extension 
 	# doesn't give us the extension's raw OID, and the shortname field returns "UNDEF", 
 	# so we access the CSR extension requests by their index
-	
 	extensions = csr.get_extensions()
-	uuid = extensions[0].get_data()[2:].lower().strip()
+	node_id = extensions[0].get_data()[2:].lower().strip()
 	cloud_platform = extensions[1].get_data()[2:].lower().strip()
 
+	# read in config file for api calls
 	f = open('config.yml')
 	secrets = yaml.safe_load(f)
 	f.close()
+	
+	if cloud_platform == "vmware":	
+		# disable SSL certificate verification for self-signed certificates on VSphere server
+		context = ssl.SSLContext(ssl.PROTOCOL_TLSv1) 
+		context.verify_mode = ssl.CERT_NONE 
 
-	if cloud_platform == "vmware":
-
-		service_instance = SmartConnect(host=secrets['vsphere']['api'], user=secrets['vsphere']['user'], pwd=secrets['vsphere']['password'], sslContext=context)
+		service_instance = SmartConnect(host=secrets['vsphere']['vsphere_server'], user=secrets['vsphere']['user'], pwd=secrets['vsphere']['password'], sslContext=context)
 		atexit.register(Disconnect, service_instance)
 
 		search_index = service_instance.content.searchIndex
 
-		vm = search_index.FindByUuid(None, uuid, True, False)			
+		vm = search_index.FindByUuid(None, node_id, True, False)			
 		
 		if vm == None:
         		exit(1)
@@ -59,8 +60,24 @@ def main():
 				exit(1)
 
 	elif cloud_platform == "aws":
-		## to do when we get up and running in aws
-		exit(1)
+		# check that the incoming name on the cert matches the 'Name' tag value on the ec2 instance
+		session = boto3.Session(aws_access_key_id=secrets['aws']['access_key_id'],aws_secret_access_key=secrets['aws']['secret_access_key'],region_name=secrets['aws']['region'])
+		ec2 = session.resource('ec2')
+		instance = ec2.Instance(node_id)
+
+		# check that we get a response
+		if len(response['Reservations']) != 1:
+			exit(1)
+		else:	
+			for tag in instance.tags:
+    			if tag["Key"] == 'Name':
+        		instance_name = tag["Value"].lower()
+
+			if instance_name == puppet_node:
+				exit(0)
+			else:
+				exit(1)
+
 	else:
 		exit(1)
 
